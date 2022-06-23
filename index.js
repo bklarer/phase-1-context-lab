f07a07009605f02170c4f91d717a358f5fd0c763
Benjamin Klarer <benjaminklarer@gmail.com>
2022-06-22 19:32:10 -0700

    commit tree ad7a2a4717b9984b656544fc022e716df227a5da

--- index.js
+++ index.js
@@ -9,6 +9,71 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(array) {
+    return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array) {
+    return array.map(record => createEmployeeRecord(record))
+}
+
+const createTimeInEvent = function (dateStamp) {
+    //can use destructuring const[date, hour] = timeIn.split(" ")
+    const date = dateStamp.split(" ")[0]
+    const hour = dateStamp.split(" ")[1]
+
+
+    const timeInLog = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    
+    this.timeInEvents.push(timeInLog) //brings external context in
+
+    return this
+}
+
+const createTimeOutEvent = function (dateStamp) {
+    //can use destructuring const[date, hour] = timeIn.split(" ")
+    const date = dateStamp.split(" ")[0]
+    const hour = dateStamp.split(" ")[1]
+
+
+    const timeOutLog = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    
+    this.timeOutEvents.push(timeOutLog) //brings external context in
+
+    return this
+}
+
+function hoursWorkedOnDate (date) { 
+    const inPunches = this.timeInEvents.find(inPunch => inPunch.date === date)
+    const outPunches = this.timeOutEvents.find(outPunch => outPunch.date === date)
+
+    return(outPunches.hour - inPunches.hour) / 100
+}
+
+function wagesEarnedOnDate (date) { //need to reference the date of the record and pull hours and wage and multiply
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+
+
+
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +86,12 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName (records, firstName) {
+    return records.find(record =>record.firstName === firstName)
+}
+
+function calculatePayroll (recordsArr) { //reference all values of all dates worked by each employee, put in array, and sum the total wages
+    return recordsArr.reduce((total, record) => {
+        return total + allWagesFor.call(record)
+    }, 0)
+}
